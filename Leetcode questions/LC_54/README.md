a6810b89a0d194668e5be8c1b9d355e9e18a96a0
Akhil <akhilselukar.work@gmail.com>
2024-07-29 15:11:33 +0530

    LC_54

---
+++ Leetcode questions/LC_54/README.md
@@ -0,0 +1,10 @@
+### 54 - Spiral matrix
+
+**Question link** - [Link](https://leetcode.com/problems/spiral-matrix/description/)
+
+**Topics** - Array, 2D Array, Looping over array
+
+**Interview tips** -
+In question on leetcode the constraints are given, but before starting this question one can ask below questions to the interviewer
+to determine edge cases.
+1. 
---
+++ Leetcode questions/LC_54/src/main/java/org/akhil/Main.java
@@ -0,0 +1,49 @@
+package org.akhil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+
+        System.out.println(spiralOrder(matrix));
+    }
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int top = 0;
+        int bottom = row-1;
+        int left = 0;
+        int right = col-1;
+
+        while((left <= right) && (top <= bottom)){
+            for(int i=top; i<=right; i++){
+                ans.add(matrix[top][i]);
+            }
+            top++;
+
+            for(int i=top; i<=bottom; i++){
+                ans.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+}
