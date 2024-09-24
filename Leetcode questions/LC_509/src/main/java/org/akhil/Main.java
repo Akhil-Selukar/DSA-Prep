befba6c7cff9dcbd82a0a8538cd4c9964b8f6f75
Akhil <akhilselukar.work@gmail.com>
2024-09-24 23:01:38 +0530

    LC-509 another approach

--- Leetcode questions/LC_509/src/main/java/org/akhil/Main.java
+++ Leetcode questions/LC_509/src/main/java/org/akhil/Main.java
@@ -16,4 +16,21 @@ public class Main {
         }
         return fib(n-1)+fib(n-2);
     }
+
+//    Above problem can be solved much faster without using recursion, but it takes more space as we need to create an array of size n.
+
+    public static int fib_2(int n) {
+        if(n<=1){
+            return n;
+        }
+        int[] tempArray = new int[n+1];
+
+        tempArray[0] = 0;
+        tempArray[1] = 1;
+
+        for(int i=2; i<=tempArray.length-1; i++){
+            tempArray[i] = tempArray[i-1]+tempArray[i-2];
+        }
+        return tempArray[n];
+    }
 }
--- Leetcode questions/LC_509/src/test/java/org/akhil/MainTest.java
+++ Leetcode questions/LC_509/src/test/java/org/akhil/MainTest.java
@@ -58,4 +58,58 @@ public class MainTest {
         int n = 8;
         assertEquals(21, Main.fib(n));
     }
+
+    @Test
+    public void testFib_2_Test1(){
+        int n = 0;
+        assertEquals(0, Main.fib_2(n));
+    }
+
+    @Test
+    public void testFib_2_Test2(){
+        int n = 1;
+        assertEquals(1, Main.fib_2(n));
+    }
+
+    @Test
+    public void testFib_2_Test3(){
+        int n = 2;
+        assertEquals(1, Main.fib_2(n));
+    }
+
+    @Test
+    public void testFib_2_Test4(){
+        int n = 3;
+        assertEquals(2, Main.fib_2(n));
+    }
+
+    @Test
+    public void testFib_2_Test5(){
+        int n = 4;
+        assertEquals(3, Main.fib_2(n));
+    }
+
+    @Test
+    public void testFib_2_Test6(){
+        int n = 5;
+        assertEquals(5, Main.fib_2(n));
+    }
+
+    @Test
+    public void testFib_2_Test7(){
+        int n = 6;
+        assertEquals(8, Main.fib_2(n));
+    }
+
+    @Test
+    public void testFib_2_Test8(){
+        int n = 7;
+        assertEquals(13, Main.fib_2(n));
+    }
+
+    @Test
+    public void testFib_2_Test9(){
+        int n = 8;
+        assertEquals(21, Main.fib_2(n));
+    }
 }
