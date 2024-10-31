1125f27547c56bfb227fd0aaf063427b0db7fa6a
Akhil <akhilselukar.work@gmail.com>
2024-10-31 22:13:48 +0530

    LC-24

---
+++ Leetcode questions/LC_24/README.md
@@ -0,0 +1,12 @@
+### 24 - Swap nodes in pairs
+
+**Question link** - [Link](https://leetcode.com/problems/swap-nodes-in-pairs/description/)
+
+**Topics** - LinkedList
+
+**Interview tips** -
+In question on leetcode the constraints are given, but before starting this question one can ask below questions to the interviewer
+to determine edge cases.
+1.
+
+**Intuition** -
---
+++ Leetcode questions/LC_24/src/main/java/org/akhil/CustomLinkedList.java
@@ -0,0 +1,62 @@
+package org.akhil;
+
+public class CustomLinkedList {
+
+    private ListNode head;
+    private ListNode tail;
+
+    public void addElementAtEnd(int value){
+        ListNode node = new ListNode(value);
+        if(this.head == null){
+            this.head = node;
+            this.tail = node;
+            return;
+        }
+        this.tail.next = node;
+        this.tail = node;
+    }
+
+    public void display(){
+        ListNode temp = this.head;
+        while(temp != null){
+            System.out.print(temp.value+" -> ");
+            temp = temp.next;
+        }
+        System.out.print("END");
+        System.out.println();
+    }
+
+    public void swapPairs() {
+        if(this.head == null || this.head.next == null){
+//            return this.head;
+            return;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode previousNode = dummy;
+        ListNode currentNode = this.head;
+        ListNode nextNode = currentNode.next;
+
+
+        while(currentNode != null && currentNode.next != null){
+            currentNode.next = nextNode.next;
+            previousNode.next = nextNode;
+            nextNode.next = currentNode;
+
+            previousNode = previousNode.next.next;
+            currentNode = currentNode.next;
+            if(nextNode.next != null && nextNode.next.next != null){
+                nextNode = nextNode.next.next.next;
+            }
+        }
+        this.head = dummy.next;
+    }
+
+    private class ListNode{
+        private int value;
+        private ListNode next;
+
+        public ListNode(int value){
+            this.value = value;
+        }
+    }
+}
