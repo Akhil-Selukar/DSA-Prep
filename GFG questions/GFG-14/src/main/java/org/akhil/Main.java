afb09eb30999a606cc87b4476554bcd3fb214c79
Akhil <akhilselukar.work@gmail.com>
2024-12-27 00:51:25 +0530

    GFG-14

--- GFG questions/GFG-14/src/main/java/org/akhil/Main.java
+++ GFG questions/GFG-14/src/main/java/org/akhil/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int noOfRows = 4;
         int noOfColumns = 5;
-        int[][] A = {{0,0},{1,1},{2,2},{3,3}};
-
+//        int[][] A = {{0,0},{1,1},{2,2},{3,3}};
+        int[][] A = {{1,1},{0,1},{3,3},{3,4}};
         Solution solution = new Solution();
 
         System.out.println(solution.numOfIslands(noOfRows, noOfColumns, A));
--- GFG questions/GFG-14/src/main/java/org/akhil/Solution.java
+++ GFG questions/GFG-14/src/main/java/org/akhil/Solution.java
@@ -24,8 +24,26 @@ public class Solution {
             count++;
 
             for(int[] direction:directions){
-                int adjRow = row + di
+                int adjRow = row + direction[0];
+                int adjCol = col + direction[1];
+
+                if(isValid(adjRow, adjCol, rows, cols)){
+                    if(visited[adjRow][adjCol]){
+                        int originalNodeNo = row * cols + col;      // 1 row means column number of elements (i.e. m elements) hence n rows means n*m elements + col extra elements.
+                        int adjacentNodeNo = adjRow * cols + adjCol;
+                        if(ds.findParent(originalNodeNo) != ds.findParent(adjacentNodeNo)){
+                            count--;
+                            ds.unionBySize(originalNodeNo, adjacentNodeNo);
+                        }
+                    }
+                }
             }
+            answer.add(count);
         }
+        return answer;
+    }
+
+    private boolean isValid(int adjRow, int adjCol, int rows, int cols) {
+        return (adjRow >= 0 && adjCol >= 0 && adjRow < rows && adjCol < cols);
     }
 }
