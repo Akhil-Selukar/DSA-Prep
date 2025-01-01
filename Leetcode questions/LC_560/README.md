65958abbdb22514a684fabf405433f818c21f688
Akhil <akhilselukar.work@gmail.com>
2025-01-01 16:33:37 +0530

    LC-560

---
+++ Leetcode questions/LC_560/README.md
@@ -0,0 +1,12 @@
+### 560 - Subarray sum equals K
+
+**Question link** - [Link](https://leetcode.com/problems/subarray-sum-equals-k/description/)
+
+**Topics** - Arrays
+
+**Interview tips** -
+In question on leetcode the constraints are given, but before starting this question one can ask below questions to the interviewer
+to determine edge cases.
+1.
+
+**Intuition** -<br>
---
+++ Leetcode questions/LC_560/src/main/java/org/akhil/Main.java
@@ -0,0 +1,27 @@
+package org.akhil;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,2,3,4};
+//        int[] arr = new int[] {1,-1,0};
+        int k = 4;
+
+        System.out.println(subarraySum(arr, k));
+    }
+
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap< Integer, Integer > map = new HashMap < > ();
+        map.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
