364ada4d8d52ed16fd772f8aaa97ddd45ebfe2aa
Akhil <akhilselukar.work@gmail.com>
2025-01-12 16:08:40 +0530

    LC-300

--- Leetcode questions/LC_300/src/main/java/org/akhil/Main.java
+++ Leetcode questions/LC_300/src/main/java/org/akhil/Main.java
@@ -2,8 +2,8 @@ package org.akhil;
 
 public class Main {
     public static void main(String[] args) {
-//        int[] nums = {10,9,2,5,3,7,101,18};
-        int[] nums = {0,1,0,3,2,3};
+        int[] nums = {10,9,2,5,3,7,101,18};     // 4
+//        int[] nums = {0,1,0,3,2,3};   // 4
 
         Solution solution = new Solution();
         System.out.println(solution.lengthOfLIS(nums));
--- Leetcode questions/LC_300/src/main/java/org/akhil/Solution.java
+++ Leetcode questions/LC_300/src/main/java/org/akhil/Solution.java
@@ -24,28 +24,51 @@ public class Solution {
 //    }
 
     // Solution 2 - Memoization
+//    public int lengthOfLIS(int[] nums) {
+//        int[][] dp = new int[nums.length][nums.length+1];
+//        for(int[] arr:dp){
+//            Arrays.fill(arr, -1);
+//        }
+//        return helper(nums, 0, -1, dp);
+//    }
+//
+//    private int helper(int[] nums, int index, int previousIndex, int[][] dp) {
+//        if(index == nums.length){
+//            return 0;
+//        }
+//
+//        if(dp[index][previousIndex+1] != -1){
+//            return dp[index][previousIndex+1];
+//        }
+//        int take= 0;
+//        if(previousIndex == -1 || nums[previousIndex]<nums[index]) {
+//            take = 1+helper(nums, index+1, index, dp);
+//        }
+//        int notTake = helper(nums, index+1, previousIndex, dp);
+//
+//        return dp[index][previousIndex+1] = Math.max(take, notTake);
+//    }
+
+    // Solution 3 - Tabulation
     public int lengthOfLIS(int[] nums) {
-        int[][] dp = new int[nums.length][nums.length+1];
-        for(int[] arr:dp){
-            Arrays.fill(arr, -1);
+        int n = nums.length;
+        if(n == 1){
+            return 1;
         }
-        return helper(nums, 0, -1, dp);
-    }
+        int[] dp = new int[n];      // to store length of LIS till specific index
+        Arrays.fill(dp, 1);
+        int maxLength = 0;
 
-    private int helper(int[] nums, int index, int previousIndex, int[][] dp) {
-        if(index == nums.length){
-            return 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[i] > nums[j]){
+                    if(dp[i] < dp[j]+1){
+                        dp[i] = dp[j]+1;
+                    }
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
         }
-
-        if(dp[index][previousIndex+1] != -1){
-            return dp[index][previousIndex+1];
-        }
-        int take= 0;
-        if(previousIndex == -1 || nums[previousIndex]<nums[index]) {
-            take = 1+helper(nums, index+1, index, dp);
-        }
-        int notTake = helper(nums, index+1, previousIndex, dp);
-
-        return dp[index][previousIndex+1] = Math.max(take, notTake);
+        return maxLength;
     }
 }
