8af49f5a2e2f5d86fc23707d100307df885f70de
Akhil <akhilselukar.work@gmail.com>
2025-01-19 14:20:05 +0530

    LC-36

---
+++ Leetcode questions/LC_36/README.md
@@ -0,0 +1,12 @@
+### 36 - Valid sudoku
+
+**Question link** - [Link](https://leetcode.com/problems/valid-sudoku/description/)
+
+**Topics** - 2D-Array
+
+**Interview tips** -
+In question on leetcode the constraints are given, but before starting this question one can ask below questions to the interviewer
+to determine edge cases.
+1.
+
+**Intuition** -
--- Leetcode questions/LC_36/src/main/java/org/akhil/Solution.java
+++ Leetcode questions/LC_36/src/main/java/org/akhil/Solution.java
@@ -1,2 +1,51 @@
-package org.akhil;public class Solution {
+package org.akhil;
+
+import java.util.Set;
+
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board.length; j++){
+                if(board[i][j] != '.'){
+                    if(!isSafe(board, i, j, board[i][j])){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isSafe(char[][] board, int row, int col, char num) {
+        // row safe
+        for(int i=0; i<board.length; i++){
+            if(i != col && board[row][i] == num){
+                return false;
+            }
+        }
+
+        // column safe
+        for(int i=0; i<board.length; i++){
+            if(i!=row && board[i][col] == num){
+                return false;
+            }
+        }
+
+        // 3X3 board safe
+        int sqrt = (int)Math.sqrt(board.length);
+        int rowStartIndex = row - row%sqrt;
+        int colStartIndex = col - col%sqrt;
+
+        int rowEndIndex = rowStartIndex + sqrt;
+        int colEndIndex = colStartIndex + sqrt;
+
+        for(int i=rowStartIndex; i<rowEndIndex; i++){
+            for(int j=colStartIndex; j<colEndIndex; j++){
+                if(i != row && j != col && board[i][j] == num){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
