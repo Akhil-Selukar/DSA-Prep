0d1e9d13dc1bd39f026d62dbe790cf1ddde0aa3a
Akhil <akhilselukar.work@gmail.com>
2025-01-30 00:13:30 +0530

    LC-215 without sorting

--- Leetcode questions/LC_215/src/main/java/org/akhil/Solution.java
+++ Leetcode questions/LC_215/src/main/java/org/akhil/Solution.java
@@ -1,11 +1,29 @@
 package org.akhil;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
 
-        return nums[nums.length-k];
+    // Solution 1 - With sorting    (O(Nlog(N))/ (whatever space required for sorting)
+//    public int findKthLargest(int[] nums, int k) {
+//        Arrays.sort(nums);
+//
+//        return nums[nums.length-k];
+//    }
+
+    // Solution 2 - Without sorting     (O(Nlog(k))/ O(k)
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue();
+
+        for(int num:nums){
+            queue.offer(num);
+//            System.out.println("Adding "+num);
+            if(queue.size() > k){
+                queue.poll();
+//                System.out.println("pooled => "+queue.poll());
+            }
+        }
+        return queue.poll();
     }
 }
