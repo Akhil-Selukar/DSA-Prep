78303ecc06cadb94b166e442dc8a7360f17a7632
Akhil <akhilselukar.work@gmail.com>
2025-02-16 03:32:25 +0530

    LC-387

---
+++ Leetcode questions/LC_387/src/main/java/org/akhil/Main.java
@@ -0,0 +1,11 @@
+package org.akhil;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "loveleetcode";
+
+        Solution solution =new Solution();
+
+        System.out.println(solution.firstUniqChar(s));
+    }
+}
