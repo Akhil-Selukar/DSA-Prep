6505fd740490ba99ab2ce45f87217ee2cd67eb9e
Akhil <akhilselukar.work@gmail.com>
2025-03-16 00:49:15 +0530

    Tree revision

--- Leetcode questions/LC_101/src/main/java/org/akhil/Main.java
+++ Leetcode questions/LC_101/src/main/java/org/akhil/Main.java
@@ -11,6 +11,8 @@ public class Main {
     // Below solution is difficult to run locally and wil need a lot of background code hence writing only solution below
     // to check the solution against testcases run the solution on leetcode.
 
+
+    // Solution 1 - O(N)/O(N)
     public boolean isSymmetric(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList();
 
@@ -42,5 +44,27 @@ public class Main {
             queue.offer(rightNode.left);
         }
         return true;        // if none of the false condition is executed and entire tree is traversed that means tree is a mirror/symmetric tree.
+    }
+
+    // Solution 2 - O(N)/O(logN)
+    public boolean isSymmetric2(TreeNode root) {
+
+        return symmetricHelper(root.left, root.right);
+    }
+
+    private boolean symmetricHelper(TreeNode left, TreeNode right) {
+        if(right == null && left == null){
+            return true;
+        }
+
+        if(right == null || left == null){
+            return false;
+        }
+
+        if(right.val != left.val){
+            return false;
+        }
+
+        return symmetricHelper(left.left, right.right) && symmetricHelper(left.right, right.left);
     }
 }
