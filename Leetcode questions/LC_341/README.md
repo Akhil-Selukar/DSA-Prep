914f4bad7e7f88268f16685598a08d35cdd75e70
Akhil <akhilselukar.work@gmail.com>
2025-03-23 15:25:04 +0530

    LC-341

---
+++ Leetcode questions/LC_341/README.md
@@ -0,0 +1,12 @@
+### 341 - Flatten Nested List Iterator
+
+**Question link** - [Link](https://leetcode.com/problems/flatten-nested-list-iterator/description/)
+
+**Topics** - Stack
+
+**Interview tips** -
+In question on leetcode the constraints are given, but before starting this question one can ask below questions to the interviewer
+to determine edge cases.
+1.
+
+**Intuition** -
---
+++ Leetcode questions/LC_341/src/main/java/org/akhil/NestedIterator.java
@@ -0,0 +1,37 @@
+package org.akhil;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+public class NestedIterator implements Iterator<Integer> {
+    Stack<NestedInteger> stack = new Stack<>();
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        for(int i=nestedList.size()-1; i>=0; i--){
+            stack.push(nestedList.get(i));
+        }
+    }
+
+    @Override
+    public Integer next() {
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while(!stack.isEmpty()){
+            NestedInteger curr = stack.peek();
+            if(curr.isInteger()){
+                return true;
+            } else {
+                stack.pop();
+                List<NestedInteger> list = curr.getList();
+                for(int i=list.size()-1; i>=0;i--){
+                    stack.push(list.get(i));
+                }
+            }
+        }
+        return false;
+    }
+}
