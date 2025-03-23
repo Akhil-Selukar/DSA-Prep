a332cf438db14f2855004a736d6127461c308345
Akhil <akhilselukar.work@gmail.com>
2025-03-23 22:27:30 +0530

    LC-404

---
+++ Leetcode questions/LC_404/README.md
@@ -0,0 +1,12 @@
+### 404 - Sum of Left Leaves
+
+**Question link** - [Link](https://leetcode.com/problems/sum-of-left-leaves/description/)
+
+**Topics** - Tree, DFS
+
+**Interview tips** -
+In question on leetcode the constraints are given, but before starting this question one can ask below questions to the interviewer
+to determine edge cases.
+1.
+
+**Intuition** -
---
+++ Leetcode questions/LC_404/src/main/java/org/akhil/Solution.java
@@ -0,0 +1,37 @@
+package org.akhil;
+
+public class Solution {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null || (root.left == null && root.right == null)){
+            return 0;
+        }
+        return helper(root, false);
+    }
+
+    private int helper(TreeNode root, boolean isLeft){
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null && root.right == null){
+            return isLeft ? root.val : 0;
+        }
+
+        int leftSum = helper(root.left, true);
+        int rightSum = helper(root.right, false);
+
+        return leftSum + rightSum;
+    }
+}
