aeef5f646d465643e57de4a9f0d86d760466baf7
Akhil <akhilselukar.work@gmail.com>
2025-04-03 22:33:00 +0530

    LC-872

---
+++ Leetcode questions/LC_872/README.md
@@ -0,0 +1,12 @@
+### 872 - Leaf similar trees
+
+**Question link** - [Link](https://leetcode.com/problems/leaf-similar-trees/description/)
+
+**Topics** - Tree, DFS
+
+**Interview tips** -
+In question on leetcode the constraints are given, but before starting this question one can ask below questions to the interviewer
+to determine edge cases.
+1.
+
+**Intuition** -
---
+++ Leetcode questions/LC_872/src/main/java/org/akhil/Solution.java
@@ -0,0 +1,30 @@
+package org.akhil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = new ArrayList();
+        List<Integer> l2 = new ArrayList();
+
+        dfs(root1, l1);
+        dfs(root2, l2);
+
+        return l1.equals(l2);
+    }
+
+    private void dfs(TreeNode root, List<Integer> list) {
+        if(root == null){
+            return;
+        }
+
+        if(root.left == null && root.right == null){
+            list.add(root.val);
+            return;
+        }
+
+        dfs(root.left, list);
+        dfs(root.right, list);
+    }
+}
