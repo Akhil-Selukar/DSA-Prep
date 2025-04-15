c6a985cdb58750988672047f8eb39fc7fd97a930
Akhil <akhilselukar.work@gmail.com>
2025-04-15 23:57:45 +0530

    LC-1534

---
+++ Leetcode questions/LC_1534/README.md
@@ -0,0 +1,12 @@
+### 1534 - Count Good Triplets
+
+**Question link** - [Link](https://leetcode.com/problems/count-good-triplets/description/)
+
+**Topics** - Array
+
+**Interview tips** -
+In question on leetcode the constraints are given, but before starting this question one can ask below questions to the interviewer
+to determine edge cases.
+1.
+
+**Intuition** -
--- Leetcode questions/LC_1534/src/main/java/org/akhil/Main.java
+++ Leetcode questions/LC_1534/src/main/java/org/akhil/Main.java
@@ -2,6 +2,12 @@ package org.akhil;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {3, 0, 1, 1, 9, 7};
+        int a = 7;
+        int b = 2;
+        int c = 3;
+
+        Solution solution = new Solution();
+        System.out.println(solution.countGoodTriplets(arr, a, b, c));
     }
 }
