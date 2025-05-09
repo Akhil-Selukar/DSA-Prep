ccbdafa235bb983ab33b3af9249fd3a62bde0d83
Akhil <akhilselukar.work@gmail.com>
2025-05-09 21:29:11 +0530

    LC-3343

--- Leetcode questions/LC_1462/src/main/java/org/akhil/Solution.java
+++ Leetcode questions/LC_1462/src/main/java/org/akhil/Solution.java
@@ -18,27 +18,27 @@ public class Solution {
         List<List<Integer>> adj = new ArrayList<>();
         int[] inDegree = new int[numCourses];
 
-        for(int i=0; i<numCourses; i++){
+        for(int i=0; i<numCourses; i++){        // O(N)
             adj.add(new ArrayList<>());
         }
-        for(int[] edge:prerequisites){
+        for(int[] edge:prerequisites){          // O(P)
             adj.get(edge[0]).add(edge[1]);
             inDegree[edge[1]]++;
             answers[edge[0]][edge[1]] = true;
         }
 
         Queue<Integer> queue = new LinkedList<>();
-        for(int i=0; i<numCourses; i++){
+        for(int i=0; i<numCourses; i++){            // O(N)
             if(inDegree[i] == 0){
                 queue.offer(i);
             }
         }
 
-        while(!queue.isEmpty()){
+        while(!queue.isEmpty()){            // O(N) will visit each Node/Vertex exactly once
             int polled = queue.poll();
 
-            for(int nei:adj.get(polled)) {
-                for (int i = 0; i < numCourses; i++) {
+            for(int nei:adj.get(polled)) {          // O(N)     here as we are not maintaining any visited array hence this loop can execute O(N) times in worst case
+                for (int i = 0; i < numCourses; i++) {      // O(N)
                     if(answers[i][polled]){
                         answers[i][nei] = true;
                     }
@@ -50,9 +50,9 @@ public class Solution {
             }
         }
         List<Boolean> res = new ArrayList<>();
-        for(int i=0; i<queries.length; i++){
-            int u = queries[i][0];
-            int v = queries[i][1];
+        for (int[] query : queries) {        // O(Q)
+            int u = query[0];
+            int v = query[1];
 
             res.add(answers[u][v]);
         }
---
+++ Leetcode questions/LC_3343/README.md
@@ -0,0 +1,12 @@
+### 3343 - Count Number of Balanced Permutations
+
+**Question link** - [Link](https://leetcode.com/problems/count-number-of-balanced-permutations/description/)
+
+**Topics** - DP, 0-1 knapsack, Maths
+
+**Interview tips** -
+In question on leetcode the constraints are given, but before starting this question one can ask below questions to the interviewer
+to determine edge cases.
+1.
+
+**Intuition** -
