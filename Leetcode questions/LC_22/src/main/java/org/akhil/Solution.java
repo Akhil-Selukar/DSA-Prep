584435eecc8635679de587f1905a633144b48b13
Akhil <akhilselukar.work@gmail.com>
2025-05-24 00:59:16 +0530

    faster approach for LC-22

--- Leetcode questions/LC_22/src/main/java/org/akhil/Solution.java
+++ Leetcode questions/LC_22/src/main/java/org/akhil/Solution.java
@@ -4,27 +4,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
+
+    // Solution 1 - using String (as String is immutable hence we dont need to backtrack here, but as every time new String object is created hence the solution is little bit slow and memory consuming even though time and space complexity will be same)
+//    public List<String> generateParenthesis(int n) {
+//        List<String> answer = new ArrayList<>();
+//        int openP = 0;
+//        int closeP = 0;
+//        helper(n, openP, closeP, "", answer);
+//
+//        return answer;
+//    }
+//
+//    private void helper(int n, int openP, int closeP, String temp, List<String> answer) {
+//        if(openP == n && closeP == n){
+//            answer.add(temp);
+//            return;
+//        }
+//
+//        if(openP < n){
+//            helper(n, openP+1, closeP, temp + "(", answer);
+//        }
+//
+//        if(closeP < openP){
+//            helper(n, openP, closeP+1, temp + ")", answer);
+//        }
+//    }
+
+    // Solution 2 - Using stringBuilder (here backtracking will be required)
     public List<String> generateParenthesis(int n) {
         List<String> answer = new ArrayList<>();
-        int openP = 0;
-        int closeP = 0;
-        helper(n, openP, closeP, "", answer);
+        StringBuilder sb = new StringBuilder();
+        helper(n, 0, 0, sb, answer);
 
         return answer;
     }
 
-    private void helper(int n, int openP, int closeP, String temp, List<String> answer) {
+    private void helper(int n, int openP, int closeP, StringBuilder sb, List<String> answer) {
         if(openP == n && closeP == n){
-            answer.add(temp);
+            answer.add(sb.toString());
             return;
         }
 
         if(openP < n){
-            helper(n, openP+1, closeP, temp + "(", answer);
+            helper(n, openP+1, closeP, sb.append("("), answer);
+            sb.deleteCharAt(sb.length()-1);
         }
 
         if(closeP < openP){
-            helper(n, openP, closeP+1, temp + ")", answer);
+            helper(n, openP, closeP+1, sb.append(")"), answer);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 }
--- Leetcode questions/LC_3068/src/main/java/org/akhil/Solution.java
+++ Leetcode questions/LC_3068/src/main/java/org/akhil/Solution.java
@@ -7,11 +7,11 @@ public class Solution {
         long discardedNode = Integer.MAX_VALUE; // Minimize the loss due to not finding XOR (Only applicable for ODD case)
 
         for (int num : nums) {
-            ans += Math.max(num, num ^ k);
-            count += (num ^ k) > num ? 1 : 0;
-            discardedNode = Math.min(discardedNode, Math.abs(num - (num ^ k)));
+            ans += Math.max(num, num ^ k);      // take the max value of node either XOR'ed or without XOR'ed
+            count += (num ^ k) > num ? 1 : 0;   // count of number of XOR operation taken (as 1 edge = 2 XOR operation)
+            discardedNode = Math.min(discardedNode, Math.abs(num - (num ^ k)));         // if in case after XORing some node is getting less value but we have to take than node because at other end of that nodes edge we are getting maximum value. In this case we need to subtract this loss from final max (as we are just maximizing the ans without considering this case)
         }
 
-        return count % 2 == 0 ? ans : (ans - discardedNode);
+        return count % 2 == 0 ? ans : (ans - discardedNode);       // if count is odd that means there is some edge which when XOR'ed gives less value at one of the two edges. Hence reduce that value.
     }
 }
