f5b68df5aad58b744e512394278780ad4436a0f2
Akhil <akhilselukar.work@gmail.com>
2025-06-29 02:31:46 +0530

    LC-46 slightly improved code

--- Leetcode questions/LC_46/src/main/java/org/akhil/Solution.java
+++ Leetcode questions/LC_46/src/main/java/org/akhil/Solution.java
@@ -4,26 +4,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> answer = new ArrayList<>();
 
-        backtrack(nums, answer, new ArrayList<Integer>());
-        return answer;
+    // Solution 1 - O(N! * N^2)/O(N! * N)
+//    public List<List<Integer>> permute(int[] nums) {
+//        List<List<Integer>> answer = new ArrayList<>();
+//
+//        backtrack(nums, answer, new ArrayList<Integer>());    // N! permutations are possible.
+//        return answer;
+//    }
+//
+//    private void backtrack(int[] nums, List<List<Integer>> answer, List<Integer> tempList) {
+//        if(tempList.size() == nums.length){
+//            answer.add(new ArrayList<>(tempList));        // O(N)
+//            return;
+//        }
+//
+//        for(int i=0; i<nums.length; i++){
+//            if(tempList.contains(nums[i])){               // O(N)
+//                continue;
+//            }
+//            tempList.add(nums[i]);
+//            backtrack(nums, answer, tempList);
+//            tempList.remove(tempList.size()-1);
+//        }
+//    }
+
+
+
+    // Solution 2 - Same concept but slightly better approach which reduce contains operation
+    // O(N! * N)/ O(N! * N)
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] considered = new boolean[nums.length];
+
+        helper(nums, new ArrayList<>(), result, considered);        // N! permutations are possible
+
+        return result;
     }
 
-    private void backtrack(int[] nums, List<List<Integer>> answer, List<Integer> tempList) {
-        if(tempList.size() == nums.length){
-            answer.add(new ArrayList<>(tempList));
+    private void helper(int[] nums, List<Integer> curr, List<List<Integer>> result, boolean[] considered){
+        if(curr.size() == nums.length){
+            result.add(new ArrayList<>(curr));          // O(N)
             return;
         }
 
         for(int i=0; i<nums.length; i++){
-            if(tempList.contains(nums[i])){
+            if(considered[i]){                          // O(1)
                 continue;
             }
-            tempList.add(nums[i]);
-            backtrack(nums, answer, tempList);
-            tempList.remove(tempList.size()-1);
+
+            curr.add(nums[i]);
+            considered[i] = true;
+            helper(nums, curr, result, considered);
+            curr.remove(curr.size()-1);
+            considered[i] = false;
         }
     }
 }
