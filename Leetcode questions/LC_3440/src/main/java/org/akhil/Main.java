edc24ddd4a7903f3cabb2a9fe43a80fb802bfc8b
Akhil <akhilselukar.work@gmail.com>
2025-07-10 23:08:13 +0530

    LC-3440

---
+++ Leetcode questions/LC_3440/src/main/java/org/akhil/Main.java
@@ -0,0 +1,13 @@
+package org.akhil;
+
+public class Main {
+    public static void main(String[] args) {
+        int eventTime = 10;
+        int[] startTime = {0,3,7,9};
+        int[] endTime = {1,4,8,10};
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.maxFreeTime(eventTime, startTime, endTime));
+    }
+}
---
+++ Leetcode questions/LC_3440/src/main/java/org/akhil/Solution.java
@@ -0,0 +1,47 @@
+package org.akhil;
+
+public class Solution {
+    public int maxFreeTime(int eventTime, int[] startTime, int[] endTime) {
+
+        int n = startTime.length;
+        int[] freeTime = new int[n+1];
+        int lastEnd = 0;
+        // calculate free time at every index
+        for(int i=0; i<n; i++){
+            freeTime[i] = startTime[i] - lastEnd;
+            lastEnd = endTime[i];
+        }
+
+        // if after last event we have some hrs left
+        freeTime[n] = eventTime - lastEnd;
+
+        int[] maxFreeToRight = new int[n+1];
+        int[] maxFreeToLeft = new int[n+1];
+        int m = maxFreeToRight.length;
+
+        for(int i=m-2; i>=0; i--){
+            maxFreeToRight[i] = Math.max(maxFreeToRight[i + 1], freeTime[i + 1]);
+        }
+
+        // precomputation of maxFreeToLeft
+        for(int i=1; i<m; i++){
+            maxFreeToLeft[i] = Math.max(maxFreeToLeft[i - 1], freeTime[i - 1]);
+        }
+
+        int result = 0;
+        // Step 4: Iterate for each possible movement/shift
+        for (int i = 1; i < m; i++) {
+            int currEventTime = endTime[i - 1] - startTime[i - 1];
+
+            // Case 1: Move completely out
+            if (currEventTime <= Math.max(maxFreeToLeft[i - 1], maxFreeToRight[i])) {
+                result = Math.max(result, freeTime[i - 1] + currEventTime + freeTime[i]);
+            }
+
+            // Case 2: Shift left or right
+            result = Math.max(result, freeTime[i - 1] + freeTime[i]);
+        }
+
+        return result;
+    }
+}
