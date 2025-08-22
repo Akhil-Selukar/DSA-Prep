8c3b92ce562bb0c83758dcc69d4da83b3952a96c
Akhil <akhilselukar.work@gmail.com>
2025-08-23 02:29:15 +0530

    LC-395

--- Leetcode questions/LC_395/src/main/java/org/akhil/Solution.java
+++ Leetcode questions/LC_395/src/main/java/org/akhil/Solution.java
@@ -1,5 +1,8 @@
 package org.akhil;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 //    public int longestSubstring(String s, int k) {
 //        // base cases
@@ -37,53 +40,93 @@ public class Solution {
 
     // Solution 2
 
+//    public int longestSubstring(String s, int k) {
+//        if(s == null || s.length() == 0){
+//            return 0;
+//        }
+//        if(k <= 1){
+//            return s.length();
+//        }
+//
+//        int start = 0;
+//        int end = s.length();
+//
+//        return helper(start, end, s, k);
+//    }
+//
+//    private int helper(int start, int end, String s, int k) {
+//        if(start >= end){
+//            return 0;
+//        }
+//
+//        // populate frequency
+//        int[] count = new int[26];
+//        for(int i=start; i<end; i++){
+//            count[s.charAt(i)-'a']++;
+//        }
+//
+//        // check validity
+//        boolean valid = true;
+//        for(int i=0; i<26 && valid; i++){
+//            if(count[i] > 0 && count[i] < k){
+//                valid = false;
+//            }
+//        }
+//        if(valid){      // i.e. entire string is valid string
+//            return end - start;
+//        }
+//
+//        int maxLength = 0;
+//        int tempStart = start;
+//        for(int i=start; i<end; i++){
+//            if(count[s.charAt(i)-'a'] >= k){
+//                continue;
+//            }
+//            maxLength = Math.max(maxLength, helper(tempStart, i, s, k));
+//            tempStart = i+1;
+//        }
+//
+//        maxLength = Math.max(maxLength, helper(tempStart, end, s, k));
+//        return maxLength;
+//    }
+
+    // -----------------
+
+    // Solution 3 - Most intuitive approach (divide and conquer)
+    // concept is as we want the resultant string such that all chars in that string must appear at least k times (which means no character in string we want
+    // can have frequency less than k) so we keep on splitting the given string recursively at on the character which has frequency less than 'k'
+    // at a point we will reach either to the required resultant string or empty string that is length 0. (in case no char has at-least k count.
+
     public int longestSubstring(String s, int k) {
-        if(s == null || s.length() == 0){
-            return 0;
-        }
-        if(k <= 1){
-            return s.length();
-        }
-
-        int start = 0;
-        int end = s.length();
-
-        return helper(start, end, s, k);
+        return solve(s, k);
     }
 
-    private int helper(int start, int end, String s, int k) {
-        if(start >= end){
+    private int solve(String s, int k) {
+        if(s.length() == 0){
             return 0;
         }
 
-        // populate frequency
-        int[] count = new int[26];
-        for(int i=start; i<end; i++){
-            count[s.charAt(i)-'a']++;
+        Map<Character, Integer> freq = new HashMap<>();
+
+        for(char ch:s.toCharArray()){
+            freq.put(ch, freq.getOrDefault(ch, 0)+1);
         }
 
-        // check validity
-        boolean valid = true;
-        for(int i=0; i<26 && valid; i++){
-            if(count[i] > 0 && count[i] < k){
-                valid = false;
+        for(char ch:freq.keySet()){
+            if(freq.get(ch) < k){
+                // split string at ch
+                String[] subStrings = s.split(String.valueOf(ch));
+                int maxLength = 0;
+                for(String str:subStrings){
+                    maxLength = Math.max(maxLength, solve(str, k));
+                }
+                return maxLength;
             }
         }
-        if(valid){      // i.e. entire string is valid string
-            return end - start;
-        }
 
-        int maxLength = 0;
-        int tempStart = start;
-        for(int i=start; i<end; i++){
-            if(count[s.charAt(i)-'a'] >= k){
-                continue;
-            }
-            maxLength = Math.max(maxLength, helper(tempStart, i, s, k));
-            tempStart = i+1;
-        }
-
-        maxLength = Math.max(maxLength, helper(tempStart, end, s, k));
-        return maxLength;
+        // if we are reaching at this point means no char in s has frequency atleat equal to k (i.e. if statement in loop was never true)
+        return s.length();
     }
+
+
 }
