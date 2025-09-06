2870f8b14d41c1a40900ba9158809fda80abe094
Akhil <akhilselukar.work@gmail.com>
2025-09-07 02:23:39 +0530

    LC-705 optimized

--- Leetcode questions/LC_705/src/main/java/org/akhil/MyHashSet.java
+++ Leetcode questions/LC_705/src/main/java/org/akhil/MyHashSet.java
@@ -3,8 +3,8 @@ package org.akhil;
 
 /*
  * IMPORTANT NOTE: Even though this is not the fastest solution on leetcode but this is the correct solution.
- * Other solution which take much less time and give correct result uses one or the other existing data structure.
- * Here the expectation is to implement this without using any data structure.
+ * We can optimize the time using Buckets concept which will reduce the iterations to contains and add method, but it is
+ * worth mentioning in interview that the optimized solution will use array data structure. (check solution 2 below)
  */
 public class MyHashSet {
     class Node{
@@ -85,3 +85,83 @@ public class MyHashSet {
         System.out.print("END");
     }
 }
+
+
+//-------------------
+// Solution 2 - with buckets logic
+
+//class MyHashSet {
+//
+//    // Node structure class
+//    private class Node{
+//        int val;
+//        Node next;
+//
+//        public Node(int val){
+//            this.val = val;
+//        }
+//    }
+//
+//    // global variable to track the head of linkedList
+//    Node[] buckets;
+//    final int BUCKET_COUNT = 1000;
+//
+//    public MyHashSet() {
+//        buckets = new Node[BUCKET_COUNT];
+//    }
+//
+//    public void add(int key) {
+//        int bucketIndex = Integer.hashCode(key) % BUCKET_COUNT;
+//        Node head = buckets[bucketIndex];
+//
+//        if(contains(key)){
+//            return;
+//        }
+//        if(head == null){
+//            head = new Node(key);
+//            buckets[bucketIndex] = head;
+//            return;
+//        }
+//
+//        while(head.next != null){
+//            head = head.next;
+//        }
+//
+//        Node newNode = new Node(key);
+//        head.next = newNode;
+//    }
+//
+//    public void remove(int key) {
+//        int bucketIndex = Integer.hashCode(key) % BUCKET_COUNT;
+//        Node head = buckets[bucketIndex];
+//
+//        // Handle case where the head node is the one to be removed
+//        if (head != null && head.val == key) {
+//            buckets[bucketIndex] = head.next;
+//            return;
+//        }
+//
+//        // Traverse to find the node and remove it
+//        Node current = head;
+//        while (current != null && current.next != null) {
+//            if (current.next.val == key) {
+//                current.next = current.next.next;
+//                return;
+//            }
+//            current = current.next;
+//        }
+//    }
+//
+//    public boolean contains(int key) {
+//        int bucketIndex = Integer.hashCode(key) % BUCKET_COUNT;
+//        Node node = buckets[bucketIndex];
+//
+//        while(node != null){
+//            if(node.val == key){
+//                return true;
+//            }
+//            node = node.next;
+//        }
+//        return false;
+//    }
+//}
