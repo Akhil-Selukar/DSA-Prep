cebeb76d9f42d2c80e08527de16642e5aa6804d7
Akhil <akhilselukar.work@gmail.com>
2025-09-10 23:55:23 +0530

    LC-173

--- Leetcode questions/LC_173/src/main/java/org/akhil/BSTIterator.java
+++ Leetcode questions/LC_173/src/main/java/org/akhil/BSTIterator.java
@@ -1,9 +1,46 @@
 package org.akhil;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class BSTIterator {
 
+//    // Solution 1 - Bruteforce
+//    // O(N)/O(N)
+//
+//    List<Integer> inOrder;
+//    int pointer;        // to track place of iterator
+//    public BSTIterator(TreeNode root) {
+//        inOrder = new ArrayList<>();
+//
+//        // perform preorder traversal and populate the list (this will be O(N))
+//        inorderTraversal(root);
+//    }
+//
+//    private void inorderTraversal(TreeNode root) {
+//        if(root == null){
+//            return;
+//        }
+//
+//        inorderTraversal(root.left);
+//        inOrder.add(root.val);
+//        inorderTraversal(root.right);
+//    }
+//
+//    public int next() {
+//        int val = inOrder.get(pointer);
+//        pointer++;
+//        return val;
+//
+//    }
+//
+//    public boolean hasNext() {
+//        return inOrder.size() > pointer;
+//    }
+
+    // -------------------------------
+    // Solution 2 - The optimized one and as per follow up
     // to keep track of smallest node at the top of stack
     Stack<TreeNode> stack = new Stack<>();
 
