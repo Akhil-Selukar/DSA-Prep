c073985e336b70ec680362bca6e29290aede8f4f
Akhil <akhilselukar.work@gmail.com>
2025-09-15 02:28:54 +0530

    LC-687

--- Leetcode questions/LC_687/src/main/java/org/akhil/Solution.java
+++ Leetcode questions/LC_687/src/main/java/org/akhil/Solution.java
@@ -3,28 +3,60 @@ package org.akhil;
 public class Solution {
 
     // Solution 1 - O(N)/O(N)
-    int maxUniLength = Integer.MIN_VALUE;
-    public int longestUnivaluePath(TreeNode root) {
-        if(root == null){
-            return 0;
-        }
+//    int maxUniLength = Integer.MIN_VALUE;
+//    public int longestUnivaluePath(TreeNode root) {
+//        if(root == null){
+//            return 0;
+//        }
+//
+//        dfsHelper(root, root.val);
+//        return maxUniLength;
+//    }
+//
+//    private int dfsHelper(TreeNode root, int prevNodeVal) {
+//        if(root == null){
+//            return 0;
+//        }
+//
+//        int leftLength = dfsHelper(root.left, root.val);
+//        int rightLength = dfsHelper(root.right, root.val);
+//
+//        maxUniLength = Math.max(maxUniLength, leftLength+rightLength);
+//        if(root.val == prevNodeVal){
+//            return Math.max(leftLength, rightLength)+1;
+//        }
+//        return 0;
+//    }
 
-        dfsHelper(root, root.val);
-        return maxUniLength;
+    // ----------------------------
+    // Solution 2 - More intuitive solution
+    // O(N)/O(H)
+    int maxLength = 0;
+    public int longestUnivaluePath(TreeNode root) {
+        dfs(root);
+        return maxLength;
     }
 
-    private int dfsHelper(TreeNode root, int prevNodeVal) {
+    private int dfs(TreeNode root){
         if(root == null){
             return 0;
         }
 
-        int leftLength = dfsHelper(root.left, root.val);
-        int rightLength = dfsHelper(root.right, root.val);
+        int left = dfs(root.left);
+        int right= dfs(root.right);
 
-        maxUniLength = Math.max(maxUniLength, leftLength+rightLength);
-        if(root.val == prevNodeVal){
-            return Math.max(leftLength, rightLength)+1;
+        int leftPathLength = 0;
+        if(root.left != null && root.val == root.left.val){
+            leftPathLength = left + 1;
         }
-        return 0;
+
+        int rightPathLength = 0;
+        if(root.right != null && root.val == root.right.val){
+            rightPathLength = right + 1;
+        }
+
+        maxLength = Math.max(maxLength, leftPathLength + rightPathLength);
+
+        return Math.max(leftPathLength, rightPathLength);
     }
 }
