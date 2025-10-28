6676affb05b7c6033e6b0f330976e028fc0ccad2
Akhil <akhilselukar.work@gmail.com>
2025-10-28 12:03:53 +0530

    LC-3354

---
+++ Battle of algorithms/Graph/G12_Kruskals_Algorithm/src/main/java/org/akhil/DisjointSet.java
@@ -0,0 +1,2 @@
+package org.akhil;public class DisjointSet {
+}
---
+++ Battle of algorithms/Graph/LC_3354/src/main/java/org/akhil/Main.java
@@ -0,0 +1,10 @@
+package org.akhil;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,0,2,0,3};
+
+        Solution solution = new Solution();
+        System.out.println(solution.countValidSelections(nums));
+    }
+}
